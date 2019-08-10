6a8095ed4074beb4605c0e44e6782041a55fe9c8
Houda Belefqih <houdabelefqih@gmail.com>
2019-08-10 15:08:43 -0500

    Added logic to HangmanGame class. All tests passing.

--- hangman/game.py
+++ hangman/game.py
@@ -50,12 +50,49 @@ class HangmanGame(object):
 
     def __init__(self, word_list=WORD_LIST, number_of_guesses=5):
         self.word_list = word_list
-        self.number_of_guesses = number_of_guesses
+        self.remaining_misses = number_of_guesses
+        self.word = GuessWord(self.select_random_word(self.word_list))
+        self.previous_guesses = []
 
-    def select_random_word(list_of_words= None):
+    @classmethod
+    def select_random_word(cls, list_of_words= None):
 
         if not list_of_words:
             raise InvalidListOfWordsException()
 
         else:
             return choice(list_of_words)
+
+    def guess(self, character):
+
+        if self.is_finished():
+            raise GameFinishedException()
+
+        else:
+            last_masked_word = self.word.masked
+            attempt = self.word.perform_attempt(character)
+
+            if character not in self.previous_guesses:
+                self.previous_guesses.append(character.lower())
+
+            if self.is_won():
+                raise GameWonException()
+
+            else:
+
+                if self.word.masked.lower() == last_masked_word.lower():
+                    self.remaining_misses -= 1
+
+                if self.is_lost():
+                    raise GameLostException()
+
+        return attempt
+
+    def is_finished(self):
+        return self.remaining_misses <= 0 or '*' not in self.word.masked
+
+    def is_won(self):
+        return self.word.masked == self.word.answer
+
+    def is_lost(self):
+        return self.word.masked != self.word.answer and self.remaining_misses == 0
